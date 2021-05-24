f26bca1887a7f12011775c8563c1677701125b09
Iyin Oyeleye <iyinoluwa.oyeleye@thebulb.africa>
2021-05-24 22:58:27 +0100

    completion of User Interface

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+function validation(value){
+    if (typeof(value) == undefined || typeof(value) == "string"|| typeof(value) == null ){
+        alert("Please input an amount in numbers"); 
+    }else {
+    }
+
+}
 
