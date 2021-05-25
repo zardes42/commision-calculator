7948b3ae2d1e033de21ebb299415f553444aa276
Iyin Oyeleye <iyinoluwa.oyeleye@thebulb.africa>
2021-05-25 10:07:12 +0100

    Editing responsiveness

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 function calc(val){
     let answer  = document.getElementById('input').value ;
 
-    if (typeof(answer) == 'undefined'|| answer =="" ){
-        document.getElementById('usr-console').innerHTML = 'Please Input a valid amount in numbers' ;
-    }else if(typeof(answer == "number")){
+    if(typeof(answer == "number")){
         
-     if (answer >= 1 && answer <= 5000){
-        document.getElementById('usr-console').innerHTML = 'Your Commission is ₦10' ;
-}
-else if(answer >= 5001 && answer <= 50000){
-    document.getElementById('usr-console').innerHTML = 'Your Commission is ₦25' ;
-}
-else if(answer >= 50001 ){
-    document.getElementById('usr-console').innerHTML = 'Your Commission is ₦50' ;
+        if (answer >= 1 && answer <= 5000){
+            document.getElementById('usr-console').innerHTML = 'Your Commission is ₦10' ;
+        }
+        else if(answer >= 5001 && answer <= 50000){
+            document.getElementById('usr-console').innerHTML = 'Your Commission is ₦25' ;
+        }
+        else if(answer >= 50001 ){
+            document.getElementById('usr-console').innerHTML = 'Your Commission is ₦50' ;
+        }
 
-
-}
-
+    }
+     if (typeof(answer) == 'undefined'|| answer =="" ){
+       document.getElementById('usr-console').innerHTML = 'Please Input a valid amount in numbers' ;
     }
 
 
