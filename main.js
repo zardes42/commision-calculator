9afe922b28e939c0cc88cb227c5437f85ae13b5a
Iyin Oyeleye <iyinoluwa.oyeleye@thebulb.africa>
2021-05-28 14:20:53 +0100

    logic implementation improvement

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-function calc(val){
-    let answer  = document.getElementById('input').value ;
-
+function calc(){
+    let answer  = Number(document.getElementById('input').value) ;
+ 
+     if (isNaN(Number(answer))){
+        document.getElementById('usr-console').innerHTML = 'Please enter an amount to transfer in numbers ' ;
+     }
     if(typeof(answer == "number")){
         
         if (answer >= 1 && answer <= 5000){
@@ -14,8 +17,8 @@ function calc(val){
         }
 
     }
-     if (typeof(answer) == 'undefined'|| answer =="" ){
-       document.getElementById('usr-console').innerHTML = 'Please Input a valid amount in numbers' ;
+     if (typeof(answer) == 'undefined'|| answer ==""){
+       document.getElementById('usr-console').innerHTML = 'Input box requires a value' ;
     }
 
 
